02b4a3c2fb8942bc272f3587f57aaf38fe71ed59
Olaf Neumann <git-account@olafneumann.org>
2021-01-03 15:51:15 +0100

    Initial Commit

---
+++ CamelCalculator/CamelCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamelCalculatorApp.swift
+//  CamelCalculator
+//
+//  Created by Olaf Neumann on 03.01.21.
+//
+
+import SwiftUI
+
+@main
+struct CamelCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
