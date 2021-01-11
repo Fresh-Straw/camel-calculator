658656c705bfc967cd4fd1b31c208a6f08a99a07
Olaf Neumann <git-account@olafneumann.org>
2021-01-11 20:24:16 +0100

    Add serious text

--- CamelCalculator/Views/NamesList.swift
+++ CamelCalculator/Views/NamesList.swift
@@ -17,12 +17,23 @@ struct NamesList: View {
                     NavigationLink(destination: PersonCreationStep1(person: .default, returnToHome: { self.returnToHome() })
                                     .navigationBarTitle("About your friend", displayMode: .inline), isActive: $navigationActive) {
                         Text("How many camels is your friend worth?")
+                            .frame(maxWidth: .infinity)
                             .padding()
                             .border(Color.black)
                     }
                 }
+                
+                
+                VStack(alignment: .center) {
+                    Text("Please consider this app as a joke.")
+                        .bold()
+                    Text("There is no serious situation in which another person, their life or their actions may be counted in camels or any other currency.")
+                        .multilineTextAlignment(.center)
+                }.foregroundColor(Color.gray)
             }
-            .navigationTitle("Camels")
+            .padding()
+            .navigationTitle("Camel Calculator")
+            .edgesIgnoringSafeArea(.bottom)
         }
     }
     
