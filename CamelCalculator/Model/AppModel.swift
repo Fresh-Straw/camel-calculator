8cb8dc309b17f4c1c7584986ab49c47ceaac2ba3
Olaf Neumann <git-account@olafneumann.org>
2021-01-13 11:46:10 +0100

    Better ID generation

--- CamelCalculator/Model/AppModel.swift
+++ CamelCalculator/Model/AppModel.swift
@@ -8,19 +8,24 @@
 import Foundation
 
 final class CamelAppModel : ObservableObject {
-    @Published var persons: [Person] = [.example1, .example2, .example3, .example4]
-    @Published var computationNavigationActive = false
-    @Published var personSortOrder: PersonSortOrder = .byResultDown
+    private static var example1 = Person(id: CamelAppModel.getNextId(), name: "Tarzan", sex: .male, age: 23, height: 175, hairColor: .black, hairLength: .shoulder, eyeColor: .green, boobSize: BoobSize.c, beard: nil, figure: .sporty)
+    private static var example2 = Person(id: CamelAppModel.getNextId(), name: "Jane", sex: .female, age: 23, height: 169, hairColor: .blond, hairLength: .long, eyeColor: .brown, boobSize: BoobSize.c, beard: nil, figure: .sporty)
+    private static var example3 = Person(id: CamelAppModel.getNextId(), name: "Claire", sex: .female, age: 30, height: 172, hairColor: .blond, hairLength: .long, eyeColor: .blue, boobSize: BoobSize.c, beard: nil, figure: .normal)
+    private static var example4 = Person(id: CamelAppModel.getNextId(), name: "Phil", sex: .male, age: 34, height: 184, hairColor: .blond, hairLength: .short, eyeColor: .blue, boobSize: nil, beard: .threeDay, figure: .thin)
     
-    private var currentId: Int = 0
+    private static var currentId: Int = 0
     
-    private func getNextId() -> Int {
+    fileprivate static func getNextId() -> Int {
         currentId += 1
         return currentId
     }
     
+    @Published var persons: [Person] = [CamelAppModel.example1, CamelAppModel.example2, CamelAppModel.example3, CamelAppModel.example4]
+    @Published var computationNavigationActive = false
+    @Published var personSortOrder: PersonSortOrder = .byResultDown
+   
     func finishPersonComputation(for person: Person) {
-        persons.append(Person(id: getNextId(), person: person))
+        persons.append(Person(id: CamelAppModel.getNextId(), person: person))
         computationNavigationActive = false
     }
     
@@ -35,22 +40,15 @@ final class CamelAppModel : ObservableObject {
     }
 }
 
-extension Person {
-    fileprivate static var example1 = Person(name: "Tarzan", sex: .male, age: 23, height: 175, hairColor: .black, hairLength: .shoulder, eyeColor: .green, boobSize: BoobSize.c, beard: nil, figure: .sporty)
-    fileprivate static var example2 = Person(name: "Jane", sex: .female, age: 23, height: 169, hairColor: .blond, hairLength: .long, eyeColor: .brown, boobSize: BoobSize.c, beard: nil, figure: .sporty)
-    fileprivate static var example3 = Person(name: "Claire", sex: .female, age: 30, height: 172, hairColor: .blond, hairLength: .long, eyeColor: .blue, boobSize: BoobSize.c, beard: nil, figure: .normal)
-    fileprivate static var example4 = Person(name: "Phil", sex: .male, age: 34, height: 184, hairColor: .blond, hairLength: .short, eyeColor: .blue, boobSize: nil, beard: .threeDay, figure: .thin)
-}
-
 struct Person: Identifiable {
-    static var `default` = example2
+    static var `default` = Person(name: "Jane", sex: .female, age: 23, height: 169, hairColor: .blond, hairLength: .long, eyeColor: .brown, boobSize: BoobSize.c, beard: nil, figure: .sporty)
     static var empty = Person(name: "", sex: nil, age: 20, height: 160, hairColor: nil, hairLength: nil, eyeColor: nil, boobSize: nil, beard: nil, figure: nil)
     
     fileprivate init(id: Int, person: Person) {
         self.init(id: id, name: person.name, sex: person.sex, age: person.age, height: person.height, hairColor: person.hairColor, hairLength: person.hairLength, eyeColor: person.eyeColor, boobSize: person.boobSize, beard: person.beard, figure: person.figure)
     }
     
-    fileprivate init(id: Int = 0, name: String, sex: Sex?, age: Double, height: Double, hairColor: HairColor?, hairLength: HairLength?, eyeColor: EyeColor?, boobSize: BoobSize?, beard: Beard?, figure: Figure?) {
+    fileprivate init(id: Int = CamelAppModel.getNextId(), name: String, sex: Sex?, age: Double, height: Double, hairColor: HairColor?, hairLength: HairLength?, eyeColor: EyeColor?, boobSize: BoobSize?, beard: Beard?, figure: Figure?) {
         self.id = id
         self.name = name
         self.sex = sex
